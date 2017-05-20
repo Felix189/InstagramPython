bc8c7e339daefab3748ad6ae25fb79b9d8762499
Centro Ático <centroatico@202C11E20168965.local>
2017-05-20 13:16:28 -0500

    cuarto commit

--- instagram/instagramapp/urls.py
+++ instagram/instagramapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.registrar, name='registro'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^crear_usuario/$', views.crear_usuario, name='crear_usuario'),
     url(r'^home/$', views.home, name='home'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -5,7 +5,22 @@ from django.shortcuts import render
 def registrar(request):
     return render(request, 'Registro.html')
 
+
+def crear_usuario( request ):
+    email = request.POST['email']
+    print (request.POST[ 'email' ])
+    print (request.POST[ 'name' ])
+    name = request.POST['name']
+    print (request.POST[ 'user' ])
+    name = request.POST['user']
+    print (request.POST[ 'password' ])
+    name = request.POST['password']
+
+    return render(request, 'Registro.html')
+
 def login(request):
     return render(request, 'login.html')
 def home(request):
     return render(request, 'home.html')
+def profile(request):
+    return render(request, 'profile.html')
