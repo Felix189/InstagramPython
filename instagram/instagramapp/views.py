c9fec7c14734956c5316d1ebe8720f323d384a01
Centro Ático <centroatico@202C11E20168965.local>
2017-05-27 12:53:50 -0500

    cuarto commit :)

--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from instagramapp.models import *
 # Create your views here.
 
 def registrar(request):
@@ -9,12 +10,20 @@ def registrar(request):
 def crear_usuario( request ):
     email = request.POST['email']
     print (request.POST[ 'email' ])
-    print (request.POST[ 'name' ])
     name = request.POST['name']
+    print (request.POST[ 'name' ])
+    user = request.POST['user']
     print (request.POST[ 'user' ])
-    name = request.POST['user']
+    password = request.POST['password']
     print (request.POST[ 'password' ])
-    name = request.POST['password']
+    user1=User.objects.create_user (username = user, email = email, first_name = name, password = password)
+
+    myUser = MiUsuario ( usuario = user1)
+    myUser.save()
+    print (user1)
+    print (user1.password)
+
+    return redirect ('login')
 
     return render(request, 'Registro.html')
 
